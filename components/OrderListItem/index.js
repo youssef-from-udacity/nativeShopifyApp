ee0f70d3e51dc338e32f7779290a17b3a1c5f7d2
Youssef Ait Boudad <youssefaitboudad@live.fr>
2021-01-07 20:43:56 +0100

    remove render default

--- components/OrderListItem/index.js
+++ components/OrderListItem/index.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import { Item, Title } from './style'
 import { Image, View, TouchableOpacity } from 'react-native'
-const renderDefault = (isDefault, onDeletePress) => {
-    if(isDefault){
-        return(
-            <View style ={{flex:1}}>
-             <Title style={{fontWeight:'bold', fontSize: 15, color:'grey', textAlign: 'right'}}>Default</Title>
-            </View>
-        )
-    }
-    return(
-        <TouchableOpacity onPress={onDeletePress} style = {{flex:1}}>
-            <Title style = {{fontSize: 15, color:'grey', textAlign: 'right'}}>Set Default</Title>
-        </TouchableOpacity>
-    )
-}
+
 export const OrderListItem = ({ address, onPressItem, onDeletePress, isDefault }) => {
     return(
         <Item onPress={onPressItem} style = {{flexDirection: 'row', backgroundColor: 'white', marginTop: 5, padding: 5}}>
